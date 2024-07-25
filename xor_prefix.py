659c854fdf4c28084df4b77fe098419909498e0f
Vishal Kumar <20105111042@dce-darbhanga.org>
2024-07-25 11:52:51 +0530

    new added
    new added

---
+++ xor_prefix.py
@@ -0,0 +1,22 @@
+arr=list(map(int,input().split()))
+n=len(arr)
+
+prefix=[0]*(n)
+
+for i in range(n):
+    if i==0:
+        prefix[i]=arr[i]
+    else:
+        prefix[i]=arr[i]^prefix[i-1]
+print(prefix)
+
+l=2
+# l=0
+r=5
+
+print(prefix[r]-prefix[l-1])
+
+if l==0:
+    print(prefix[r])
+else:
+    print(prefix[r]^prefix[l-1])
