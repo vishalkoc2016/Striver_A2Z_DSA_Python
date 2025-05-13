580144a1055df0453492b5da95bf73b1774d775d
Vishal Kumar <81177764+vishalkoc2016@users.noreply.github.com>
2025-05-13 14:28:23 +0530

    Time: 2206 ms (62.74%), Space: 18.2 MB (74.51%) - LeetHub

---
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.py
@@ -0,0 +1,17 @@
+class Solution:
+  def lengthAfterTransformations(self, s: str, t: int) -> int:
+    MOD = 1_000_000_007
+    count = [0] * 26
+
+    for c in s:
+      count[ord(c) - ord('a')] += 1
+
+    for _ in range(t):
+      newCount = [0] * 26
+      for i in range(25):
+        newCount[i + 1] = count[i]
+      newCount[0] = count[25]
+      newCount[1] = (newCount[1] + count[25]) % MOD
+      count = newCount
+
+    return sum(count) % MOD
